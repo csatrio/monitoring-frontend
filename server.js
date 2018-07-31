9112aa75c035578e8647f8decd0e966de7b41b49
DESKTOP-HDM7IU5\Satrio <constantinus.satrio@gmail.com>
2018-08-01 00:27:09 +0700

    nightly commit

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-var http = require('http')
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World\n')
-}).listen(8000, '127.0.0.1')
-console.log('Server running at http://127.0.0.1:8000/')
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({
+  port: 8000
+})
+
+wss.on('connection', function connection (ws) {
+  ws.on('message', function incoming (message) {
+    console.log('received: %s', message)
+  })
+
+  ws.send('something')
+})
